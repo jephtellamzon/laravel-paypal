72fff331f2ddfa6360c939881f1501018c7e0a01
Jephte Llamzon <62513262+jephtellamzon@users.noreply.github.com>
2023-12-28 20:20:14 +0800

    add initial product grids

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,11 +7,27 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+            <div class="grid grid-cols-1 md:grid-cols-3 grid-row-5 gap-4">
+                {{-- Product1 --}}
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        {{ __("Product 1") }}
+                    </div>
+                </div>
+                {{-- Product2 --}}
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        {{ __("Product 2") }}
+                    </div>
+                </div>
+                {{-- Product3 --}}
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        {{ __("Product 3") }}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
 </x-app-layout>
